b41650de44222c36a40fd6b95909b32893355074
Aldo Burgos <aldo.burgos@cuc.cr>
2020-09-14 15:11:52 -0600

    Docs agregado

---
+++ Docs/precache-manifest.e08a6cefa71c9e6bb549679b6d21e63a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "910c8fdd62766e99d30cdb959558dce3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca68fbac03f632494596",
+    "url": "/static/css/main.fbf74499.chunk.css"
+  },
+  {
+    "revision": "dcf09d6ede3b15119fcb",
+    "url": "/static/js/2.a16041ce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a16041ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca68fbac03f632494596",
+    "url": "/static/js/main.04909b3d.chunk.js"
+  },
+  {
+    "revision": "9d3929294d677b1b9ff6",
+    "url": "/static/js/runtime-main.aa9df6a3.js"
+  }
+]);
